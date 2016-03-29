9bf8ba0778c257cab5d4ad889df4b21a55ed156b
ikeamofa <ikeamofa14@gmail.com>
2016-03-29 09:29:50 -0700

    Added files via upload
    Created project

---
+++ Q110.java
@@ -0,0 +1,27 @@
+
+public class Q110
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Keychains3
+     */
+    public Q110()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
